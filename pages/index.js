db33307e15d5ec7b46e2d99247ef3d2f1f20de3d
winkerVSbecks <varunvachhar@gmail.com>
2022-04-25 23:55:12 -0400

    outline

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+const Home = () => (
+  <ul>
+    <li>Writing system + vowels & consonants</li>
+    <li>Types of writing systems</li>
+    <li>ABCD Family Tree</li>
+    <li>
+      Evolution of Letters
+      <ul>
+        <li>Evolution of Roman alphabet</li>
+        <li>Evolution of Cyrillic alphabet</li>
+        <li>Evolution of Devanagari</li>
+      </ul>
+    </li>
+    <li>Diacritic</li>
+    <li>Transliteration</li>
+  </ul>
+);
+
+export default Home;
